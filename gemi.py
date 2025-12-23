c675687d45a375bb364035e8d0d522d68346d0cf
moaad_isn't_my_name <78547347+moaad77@users.noreply.github.com>
2025-12-23 02:01:03 +0100

    Add delay before cleaning up cloned repository
    Added a delay before removing the cloned repository.

--- gemi.py
+++ gemi.py
@@ -1,9 +1,12 @@
 import shutil
 import webbrowser
 import os
+import time
 os.system('git clone https://github.com/moaad77/gemi_js_py.git')
 os.chdir('gemi_js_py')
 
 webbrowser.open('index.html')
 os.chdir('..')
+time.sleep(16)
 shutil.rmtree('gemi_js_py')
+
