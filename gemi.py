bcfcbd78c48b3a8f1834fd85b83b72e4ba55b425
moaad_isn't_my_name <78547347+moaad77@users.noreply.github.com>
2025-12-23 02:08:28 +0100

    Modify gemi.py to use input instead of sleep
    Replaced time.sleep with input for user control.

--- gemi.py
+++ gemi.py
@@ -1,12 +1,11 @@
 import shutil
 import webbrowser
 import os
-import time
 os.system('git clone https://github.com/moaad77/gemi_js_py.git')
 os.chdir('gemi_js_py')
 
 webbrowser.open('index.html')
-time.sleep(16)
-webbrowser.close('index.html')
+input('ENTER')
 os.chdir('..')
 shutil.rmtree('gemi_js_py')
+
