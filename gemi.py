82509381883d0aa01a182771f0b53f97ef2a031c
moaad_isn't_my_name <78547347+moaad77@users.noreply.github.com>
2025-12-23 01:48:01 +0100

    Enhance gemi.py to delete cloned repository
    Added cleanup steps to remove the cloned repository after opening the HTML file.

--- gemi.py
+++ gemi.py
@@ -2,4 +2,7 @@ import webbrowser
 import os
 os.system('git clone https://github.com/moaad77/gemi_js_py.git')
 os.chdir('gemi_js_py')
+
 webbrowser.open('index.html')
+os.chdir('..')
+os.system('del gemi_js_py')
