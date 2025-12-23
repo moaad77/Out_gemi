9794270eb9dd1e7808b10c40451770f2682ecb8a
moaad_isn't_my_name <78547347+moaad77@users.noreply.github.com>
2025-12-23 02:26:24 +0100

    Decrease sleep duration before removing index.html
    Reduced sleep time from 4 seconds to 2 seconds.

--- gemi.py
+++ gemi.py
@@ -5,10 +5,11 @@ os.system('git clone https://github.com/moaad77/gemi_js_py.git')
 os.chdir('gemi_js_py')
 webbrowser.open('index.html')
 #input('ENTER')
-time.sleep(4)
+time.sleep(2)
 os.remove('index.html')
 os.chdir('..')
 os.remove('gemi.py')
+
 
 
 
