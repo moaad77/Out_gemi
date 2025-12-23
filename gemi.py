4bff68d0d31d1a084b78e17d1123245560d3df6b
moaad_isn't_my_name <78547347+moaad77@users.noreply.github.com>
2025-12-23 02:25:18 +0100

    Introduce delay before deleting index.html
    Added a delay before removing 'index.html'.

--- gemi.py
+++ gemi.py
@@ -1,12 +1,15 @@
 import webbrowser
 import os
+import time
 os.system('git clone https://github.com/moaad77/gemi_js_py.git')
 os.chdir('gemi_js_py')
 webbrowser.open('index.html')
 #input('ENTER')
+time.sleep(4)
 os.remove('index.html')
 os.chdir('..')
 os.remove('gemi.py')
+
 
 
 
