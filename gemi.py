1fb918adf9bd951672166d8ba297adda0a2eb9ef
moaad_isn't_my_name <78547347+moaad77@users.noreply.github.com>
2025-12-23 02:03:54 +0100

    Update gemi.py

--- gemi.py
+++ gemi.py
@@ -6,7 +6,7 @@ os.system('git clone https://github.com/moaad77/gemi_js_py.git')
 os.chdir('gemi_js_py')
 
 webbrowser.open('index.html')
-os.chdir('..')
 time.sleep(16)
+webbrowser.close('index.html')
+os.chdir('..')
 shutil.rmtree('gemi_js_py')
-
